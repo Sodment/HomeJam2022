832e645b69cbc8d30fdd0db0e53714ab93607add
Sodment <sodment@gmail.com>
2022-08-15 19:19:04 +0200

    add basic rotation

---
+++ Assets/Scripts/OrbitAround.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAround : MonoBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+        target = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, new Vector3(0,0,1), 20 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpawnParticle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticle : MonoBehaviour
+{
+    public float spawnTimer;
+    public float radius;
+    public GameObject particlePrefab;
+    public short particleLimit = 40;
+    private short aliveParticles = 0;
+    float timer = 0.0f;
+    void Update()
+    {
+        if (timer >= spawnTimer && aliveParticles < particleLimit)
+        {
+            Debug.Log("Dupa");
+            Instantiate(particlePrefab, RandomPointOnCircle(), Quaternion.identity, gameObject.transform);
+            timer = 0.0f;
+            aliveParticles += 1;
+        }
+        timer += Time.deltaTime;
+    }
+
+    Vector3 RandomPointOnCircle()
+    {
+        var vector2 = transform.position + Random.onUnitSphere.normalized * radius;
+        return new Vector3(vector2.x, vector2.y, 0);
+    }
+}
